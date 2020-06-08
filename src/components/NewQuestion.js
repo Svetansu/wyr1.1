22b416a73b36a5138262f419e4e87b4a43d531fb
Svet <svetansusidhya@gmail.com>
2020-06-08 21:18:29 +0530

    added actions and basic components

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewQuestion extends Component {
+   render() {
+        return (
+            <div>
+                <h1>NewQ</h1>
+            </div>
+        )
+   }
+}
+
+export default NewQuestion
